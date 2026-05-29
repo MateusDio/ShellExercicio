5f258c8ef52c16d984e0246367985d94e288235f
MateusDio <165714347+MateusDio@users.noreply.github.com>
2026-05-29 16:33:48 -0300

    Create attv22.sh

---
+++ attv22.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Digite seu nome:"
+read nome
+
+echo "Digite sua idade:"
+read idade
+
+echo "Digite sua cidade:"
+read cidade
+
+echo "Dados"
+echo "Nome: $nome"
+echo "Idade: $idade"
+echo "Cidade: $cidade"
